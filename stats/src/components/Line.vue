11d9003bc080430b5e57d4e6cbdfdae910b78836
Jeffrey Jose <jeffjosejeff@gmail.com>
2017-01-26 17:04:49 -0500

    Testing line

--- stats/src/components/Line.vue
+++ stats/src/components/Line.vue
@@ -1,53 +1,44 @@
 <template>
-  <div class="hello">
     <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank">Forum</a></li>
-      <li><a href="https://gitter.im/vuejs/vue" target="_blank">Gitter Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank">Twitter</a></li>
-      <br>
-      <li><a href="http://vuejs-templates.github.io/webpack/" target="_blank">Docs for This Template</a></li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li><a href="http://router.vuejs.org/" target="_blank">vue-router</a></li>
-      <li><a href="http://vuex.vuejs.org/" target="_blank">vuex</a></li>
-      <li><a href="http://vue-loader.vuejs.org/" target="_blank">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a></li>
-    </ul>
-  </div>
+    <svg width="500" height="270">
+        <g style="transform: translate(0, 20px)">
+            <path :d="line"/>
+        </g>
+    </svg>
 </template>
 
-<script>
-export default {
+<script lang="coffee">
+module.exports =
   name: 'line',
-  data () {
-    return {
-      msg: 'Welcome to Your Vue.js App'
-    }
-  }
-}
+  data: () ->
+    msg: 'Hi!'
+    data: [1, 40, 23, 5, 33, 20]
+    line: ''
+  mounted: () ->
+      calculatePath()
+  methods:
+    getScales: () ->
+
+          x = d3.scaleTime().range([0, 430])
+          y = d3.scaleLinear().range([210, 0])
+          d3.axisLeft().scale(x)
+          d3.axisBottom().scale(y)
+
+          x.domain(d3.extent(@data, (d, i) => i))
+          y.domain([0, d3.max(@data, d => d)])
+
+          { x, y }
+    calculatePath: () ->
+      scale = this.getScales()
+      path = d3.line()
+        .x((d, i) => scale.x(i))
+        .y(d => scale.y(d))
+
+      @line = path(this.data)
+
+
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h1, h2 {
-  font-weight: normal;
-}
-
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-
-a {
-  color: #42b983;
-}
 </style>
